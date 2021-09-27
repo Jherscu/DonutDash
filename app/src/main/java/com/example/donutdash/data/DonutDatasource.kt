8fb5b9cba45751f2aa85516266ba8b10dc989026
Justin Herscu <jherscucode@gmail.com>
2021-09-26 22:15:32 -0400

    Day 14/15:
    - Setup basic functionality for toppings fragment recyclerview, with necessary adapter, viewholder, and function of datasource from flavor fragment
    
    - Set up breakpoints to test for flubbed functionality of chipgroup and label

--- app/src/main/java/com/example/donutdash/data/DonutDatasource.kt
+++ app/src/main/java/com/example/donutdash/data/DonutDatasource.kt
@@ -1,11 +1,35 @@
 package com.example.donutdash.data
 
+import com.example.donutdash.R
 import com.example.donutdash.model.Donut
+import com.example.donutdash.ui.main.ToppingsFragment
+import com.google.android.material.chip.ChipGroup
 
 class DonutDatasource {
-    fun loadDonuts(): List<Donut> {
-        val list = mutableListOf<Donut>()
-        /* for donut in list from flavor fragment */
-        TODO("add to list and return donut list for ToppingsFragment")
+
+    private fun getStringResourceByName(name: String): Int {
+        return when (name) {
+            "Chocolate" -> R.string.chocolate
+            "Berry" -> R.string.berry
+            "Vanilla" -> R.string.vanilla
+            "Caramel" -> R.string.caramel
+            "Taro" -> R.string.taro
+            "Churro" -> R.string.churro
+            "Lingonberry Jam" -> R.string.lingonberry_jam
+            "Boston Creme" -> R.string.boston_creme
+            "Powdered" -> R.string.powdered
+            "Apple Fritter" -> R.string.apple_fritter
+            else -> -1
+        }
+    }
+
+    fun loadDonuts(flavorMap: Map<String, Int>) : List<Donut> {
+        val donutList = mutableListOf<Donut>()
+        for (entry in flavorMap) {
+            repeat(entry.value) {
+                donutList.add(Donut(getStringResourceByName(entry.key)))
+            }
+        }
+        return donutList
     }
 }
